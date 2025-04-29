09262956fdebeceabc4375cee32634ef256b00f6
killuaZolyck69 <tanver694225@gmail.com>
2025-04-29 22:24:50 +0600

    Solve nth Fibonacci Number problem

---
+++ Step 1 Learn the basics/Lec 5 Learn Basic Recursion/Fibonacci_Number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int nthFibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    int last = nthFibonacci(n - 1);
+    int secondLast = nthFibonacci(n - 2);
+    return last + secondLast;
+}
+
+int main()
+{
+    int num;
+    cout << "Enter the nubmer: ";
+    cin >> num;
+    cout << nthFibonacci(num) << endl;
+    return 0;
+}
