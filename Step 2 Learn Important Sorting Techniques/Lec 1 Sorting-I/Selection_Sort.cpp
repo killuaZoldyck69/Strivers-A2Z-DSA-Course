bd05258d247c2e01aa40ac5f808e171c2b03b948
killuaZolyck69 <tanver694225@gmail.com>
2025-05-10 22:38:35 +0600

    Solve Selection Sort problem

---
+++ Step 2 Learn Important Sorting Techniques/Lec 1 Sorting-I/Selection_Sort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 2; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j <= n - 1; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        cout << min << " --> " << arr[min] << endl;
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
+
+int main()
+{
+    int size;
+    cout << "Enter the array size: ";
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+    selectionSort(arr, size);
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
